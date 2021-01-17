ce30a19be3755d3beb0395a2ffab3c41d3a26049
Sultan Arrose <sultanarrose@gmail.com>
2021-01-17 17:46:41 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,13 @@ Route::get('/me', 'AuthController@me');
 Route::resource('/books', 'BookController');
 
 Route::resource('/authors', 'AuthorController');
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::post('/refresh', [AuthController::class, 'refresh']);
+
+Route::get('/user-profile', [AuthController::class, 'userProfile']);   
